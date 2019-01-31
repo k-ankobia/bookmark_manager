b826fedf29e282c0479942ebadbdb72bcd919f16
Kwaku Ankobia <kwaku.ankobia@gmail.com>
2019-01-31 14:22:13 +0000

    added view page for bookmark. user can add bookmark

--- app.rb
+++ app.rb
@@ -15,6 +15,10 @@ class App < Sinatra::Base
     erb :'bookmarks/index'
   end
 
+  post '/add_bookmark' do 
+    url = params['url']
+    redirect '/bookmarks'
+  end 
   run! if app_file == $0
-
+  
 end
